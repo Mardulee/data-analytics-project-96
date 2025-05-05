0a583378c01ef1795562cbcb24152a779390ffef
Mardulee <mari.dulepova@gmail.com>
2025-05-05 09:51:41 +0500

    Create last_paid_click.sql

---
+++ last_paid_click.sql
@@ -0,0 +1,63 @@
+~~ Задание 2
+WITH paid_sessions AS (
+SELECT
+s.visitor_id,
+s.visit_date,
+s.source AS utm_source,
+s.medium AS utm_medium,
+s.campaign AS utm_campaign
+FROM sessions s
+WHERE LOWER(s.medium) IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+),
+sessions_with_leads AS (
+SELECT
+ps.*,
+l.lead_id,
+l.amount,
+l.created_at,
+l.closing_reason,
+l.status_id,
+ROW_NUMBER() OVER (
+PARTITION BY l.visitor_id
+ORDER BY ps.visit_date DESC
+) AS rn
+FROM paid_sessions ps
+JOIN leads l
+ON ps.visitor_id = l.visitor_id
+AND ps.visit_date <= l.created_at
+),
+last_paid_clicks AS (
+SELECT *
+FROM sessions_with_leads
+WHERE rn = 1
+),
+all_visits AS (
+SELECT
+s.visitor_id,
+s.visit_date,
+s.source AS utm_source,
+s.medium AS utm_medium,
+s.campaign AS utm_campaign
+FROM sessions s
+)
+SELECT
+v.visitor_id,
+v.visit_date,
+v.utm_source,
+v.utm_medium,
+v.utm_campaign,
+l.lead_id,
+l.created_at,
+l.amount,
+l.closing_reason,
+l.status_id
+FROM all_visits v
+LEFT JOIN last_paid_clicks l
+ON v.visitor_id = l.visitor_id
+AND v.visit_date = l.visit_date
+ORDER BY
+amount DESC NULLS LAST,
+visit_date ASC,
+utm_source ASC,
+utm_medium ASC,
+utm_campaign ASC;
