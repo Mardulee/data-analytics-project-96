7a2922609de2109fba6eef4a303899a22b641adf
Mardulee <mari.dulepova@gmail.com>
2025-07-09 22:23:01 +0500

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,56 +1,61 @@
 ~~ Задание 2
 WITH
 sessions_with_leads AS (
-SELECT
-s.visitor_id,
-s.visit_date,
-s.source AS utm_source,
-s.medium AS utm_medium,
-s.campaign AS utm_campaign,
-l.lead_id,
-l.amount,
-l.created_at,
-l.closing_reason,
-l.status_id,
-ROW_NUMBER() OVER (PARTITION BY l.visitor_id ORDER BY s.visit_date DESC) AS rn
-FROM sessions s
-JOIN leads l
-ON s.visitor_id = l.visitor_id
-AND s.visit_date <= l.created_at
-WHERE LOWER(s.medium) <>'organic'
+    SELECT
+        s.visitor_id,
+        s.visit_date,
+        s.source AS utm_source,
+        s.medium AS utm_medium,
+        s.campaign AS utm_campaign,
+        l.lead_id,
+        l.amount,
+        l.created_at,
+        l.closing_reason,
+        l.status_id,
+        ROW_NUMBER()
+            OVER (PARTITION BY l.visitor_id ORDER BY s.visit_date DESC)
+        AS rn
+    FROM sessions AS s
+    INNER JOIN leads AS l
+        ON
+            s.visitor_id = l.visitor_id
+            AND s.visit_date <= l.created_at
+    WHERE LOWER(s.medium) <> 'organic'
 ),
 last_paid_clicks AS (
-SELECT *
-FROM sessions_with_leads
-WHERE rn = 1
+    SELECT *
+    FROM sessions_with_leads
+    WHERE rn = 1
 ),
 all_visits AS (
-SELECT
-s.visitor_id,
-s.visit_date,
-s.source AS utm_source,
-s.medium AS utm_medium,
-s.campaign AS utm_campaign
-FROM sessions s
+    SELECT
+        s.visitor_id,
+        s.visit_date,
+        s.source AS utm_source,
+        s.medium AS utm_medium,
+        s.campaign AS utm_campaign
+    FROM sessions AS s
 )
 SELECT
-v.visitor_id,
-v.visit_date,
-v.utm_source,
-v.utm_medium,
-v.utm_campaign,
-l.lead_id,
-l.created_at,
-l.amount,
-l.closing_reason,
-l.status_id
-FROM all_visits v
-LEFT JOIN last_paid_clicks l
-ON v.visitor_id = l.visitor_id
-AND v.visit_date = l.visit_date
+    v.visitor_id,
+    v.visit_date,
+    v.utm_source,
+    v.utm_medium,
+    v.utm_campaign,
+    l.lead_id,
+    l.created_at,
+    l.amount,
+    l.closing_reason,
+    l.status_id
+FROM all_visits AS v
+LEFT JOIN last_paid_clicks AS l
+    ON
+        v.visitor_id = l.visitor_id
+        AND v.visit_date = l.visit_date
 ORDER BY
-amount DESC NULLS LAST,
-visit_date ASC,
-utm_source ASC,
-utm_medium ASC,
-utm_campaign ASC;
+    amount DESC NULLS LAST,
+    visit_date ASC,
+    utm_source ASC,
+    utm_medium ASC,
+    utm_campaign ASC
+LIMIT 10;
